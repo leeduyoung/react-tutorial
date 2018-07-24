b7322488fd80e74427798d9f6a5919ae7f797c5b
dylee <leeduyoung20@naver.com>
2018-07-24 22:34:42 +0900

    vscode react tutorial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+// ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();

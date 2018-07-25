6d1048f5718a9fb5debd6e76e782f4a7b91d52a6
이두영 <leeduyoung20@naver.com>
2018-07-25 13:45:27 +0900

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
+
+  test() {
+    console.log('test');
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +16,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React2</h1>
         </header>
+        <div>
+          <button onClick={this.test}>default</button>
+        </div>
+        <div>
+          <Button bsStyle="success">bootstrap default button</Button>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
